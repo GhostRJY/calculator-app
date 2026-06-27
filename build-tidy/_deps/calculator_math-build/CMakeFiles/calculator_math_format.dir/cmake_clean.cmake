2193bcd4e17aa858d2196e29a4584ba5a38b03ae
GhostRJY <rode1985@gmail.com>
2026-06-27 12:12:10 +0000

    clear project

--- build-tidy/_deps/calculator_math-build/CMakeFiles/calculator_math_format.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/calculator_math_format"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/calculator_math_format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
